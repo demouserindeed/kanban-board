bdc45ee1a157694dd2d84d70ae50a429ec2437ef
MG <demouserindeed@gmail.com>
2024-06-20 20:03:47 +0530

    project-setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/layout/Header/Header";
+import Footer from "./components/layout/Footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Footer/Footer.tsx
@@ -0,0 +1,6 @@
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  return <div className={styles.footer}>KANBAN BOARD FOOTER</div>;
+};
+export default Footer;
